adee41ce5b5f72864f08b316691ab4e202a90a83
gauravo7 <gaurav@o7services.com>
2025-04-23 09:34:58 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to My First React App ViaAp Services
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://o7services.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          O7 Services
         </a>
       </header>
     </div>
